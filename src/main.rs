5f29d2ea74294a22a00d705d4b76b786d665f0d3
Benjamin Massey <benjamin.w.massey@gmail.com>
2023-01-23 02:16:30 -0800

    Broke things up into functions, for clarity and usage.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use inputbot::KeybdKey::*;
-use std::time::Instant;
+use std::{time::Instant, string};
 
 fn main() {
 
@@ -7,34 +7,42 @@ fn main() {
 
     F8Key.bind(move || {
         if LShiftKey.is_pressed() && LControlKey.is_pressed() {
-            let secs_in_min = 60 as f64;
-
-            let end: f64 = initial.elapsed().as_secs() as f64;
-            let start: f64 = f64::max(end - secs_in_min, 0f64);
+            let end = initial.elapsed().as_secs() as f64;
+            let start = f64::max(end - 60f64, 0f64);
             
-            let start_min = f64::floor(start / secs_in_min);
-            let start_sec = start % secs_in_min;
+            let result = duration_str(start, end);
 
-            let end_min = f64::floor(end / secs_in_min);
-            let end_sec = end % secs_in_min;
-
-            let zero_cap = 9 as f64;
-
-            let mut min_zero_start = "0";
-            if start_min > zero_cap { min_zero_start = ""; }
-            let mut sec_zero_start = "0";
-            if start_sec > zero_cap { sec_zero_start = ""; }
-
-            let mut min_zero_end = "0";
-            if end_min > zero_cap { min_zero_end = ""; }
-            let mut sec_zero_end = "0";
-            if end_sec > zero_cap { sec_zero_end = ""; }
-
-            println!("{}{}:{}{} - {}{}:{}{}", 
-                min_zero_start, start_min, sec_zero_start, start_sec,
-                min_zero_end, end_min, sec_zero_end, end_sec);
+            println!("{}", result);
         }
     });
     
     inputbot::handle_input_events();
+}
+
+fn secs_to_mins_secs(time: f64) -> (f64, f64) {
+    let secs_in_min = 60 as f64;
+    let min = f64::floor(time / secs_in_min);
+    let sec = time % secs_in_min;
+    return (min, sec);
+}
+
+fn mins_secs_formatted(inp: (f64, f64)) -> String {
+    let zero_cap = 9 as f64;
+    let mut first_zero = "0";
+    if inp.0 > zero_cap { first_zero = ""; }
+    let mut second_zero = "0";
+    if inp.1 > zero_cap { second_zero = ""; }
+    return format!("{}{}:{}{}", 
+                    first_zero, inp.0,
+                    second_zero, inp.1);
+}
+
+fn duration_str(start: f64, end: f64) -> String {
+    let start_tuple = secs_to_mins_secs(start as f64);
+    let end_tuple = secs_to_mins_secs(end as f64);
+
+    let start_str = mins_secs_formatted(start_tuple);
+    let end_str = mins_secs_formatted(end_tuple);
+
+    return format!("{} - {}", start_str, end_str);
 }
